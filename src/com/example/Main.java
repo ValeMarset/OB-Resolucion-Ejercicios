655a4d2ded6051756a05c78d44d287655029020c
Vale Marset <valemarset05@gmail.com>
2022-11-03 19:38:14 -0300

    Ejercicio terminado

---
+++ src/com/example/Main.java
@@ -0,0 +1,40 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Tipos de Datos numericos
+        byte byte1 = 5;
+        System.out.println(byte1);
+
+        short short1 = 10;
+        System.out.println(short1);
+
+        int entero = 30;
+        System.out.println(entero);
+
+        long long1 = 100;
+        System.out.println(long1);
+
+        //Tipos de datos decimales
+        float float1 = 5.5f;
+        System.out.println(float1);
+
+        double double1 = 10.5d;
+        System.out.println(double1);
+
+        //Tipos de datos booleanos
+        boolean verdadero = true;
+        System.out.println(verdadero);
+
+        boolean falso = false;
+        System.out.println(falso);
+
+        //Tipos de datos de texto
+        char texto1 = 'a';
+        System.out.println(texto1);
+
+        String cadenaDeTexto = "Hola Mundo";
+        System.out.println(cadenaDeTexto);
+
+    }
+}
